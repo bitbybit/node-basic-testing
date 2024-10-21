75d6ed54013c0acb454f4a9b594088d32ac391f2
bitbybit <1628191+bitbybit@users.noreply.github.com>
2024-10-21 23:28:18 +0200

    feat: lib

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,43 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+
+jest.mock('lodash', () => ({
+  ...jest.requireActual('lodash'),
+  throttle: jest.fn((fn) => fn),
+}));
+
+let axiosInstance: any;
 
 describe('throttledGetDataFromApi', () => {
+  beforeEach(() => {
+    axiosInstance = {
+      get: jest.fn().mockResolvedValue({ data: 'mocked' }),
+    };
+
+    (axios.create as jest.Mock).mockReturnValue(axiosInstance);
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi('/test');
+
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi('/path/to/test');
+
+    expect(axiosInstance.get).toHaveBeenCalledWith('/path/to/test');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    axiosInstance.get.mockResolvedValueOnce({ data: 'mocked' });
+
+    const result = await throttledGetDataFromApi('/path/to/test');
+
+    expect(result).toBe('mocked');
   });
 });
